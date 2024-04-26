9312f1c7c55026b0456948cb350e70a4dd248bcf
mauro <mrb0305@gmail.com>
2024-04-26 17:50:37 -0300

    POO parte2

--- poo.py
+++ poo.py
@@ -5,3 +5,14 @@ class Pessoa:
     def __init__(self,nome,idade) -> None:
         self.nome=nome
         self.idade=idade
+
+    def saudacao(self):
+        return f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos."
+
+pessoa = Pessoa("Mauro",40)
+mensagem = pessoa.saudacao()
+print(mensagem)
+
+pessoa2 = Pessoa(nome="Rodrigo", idade=32)
+mensagem = pessoa2.saudacao()
+print(mensagem)
